93be9be4343ce5f79ed51435b9696a19af56d682
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-01 01:06:12 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ resource "aws_route" "peer" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
-resource "aws_route" "default_vpc_peer_entry" {
-  route_table_id = var.default_vpc_route_table_id
+resource "aws_route" "default-vpc-peer-entry" {
+  route_table_id         = var.default_vpc_route_table_id
   destination_cidr_block = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
