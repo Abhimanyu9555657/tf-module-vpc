c40c61b5ef53acd10c329e7eb5493d97c3ee505c
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-31 13:12:08 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  for_each                   = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
-  domain = "vpc"
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  domain   = "vpc"
 }
 
 resource "aws_nat_gateway" "ngw" {
